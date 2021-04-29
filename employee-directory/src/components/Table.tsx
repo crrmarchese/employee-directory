8bd33ac3af708430f06499ba6ef0b40578a039ac
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-04-29 13:12:06 -0400

    Remove public file from gitignore; comments for UI Kit in Table

--- employee-directory/src/components/Table.tsx
+++ employee-directory/src/components/Table.tsx
@@ -13,8 +13,8 @@ const Table = () => {
 
     // First item in array sets the initial state, second item is the function to update it
     const [employees, setEmployees] = useState([]);
-    //const productService = new ProductService();
-
+   
+    // Import the API data, create a const that holds the new array data created by .map and return the individual data items that shows in the table. Axios returns a "data" object by default but then this api has an object named "data" and the name of the array of data returned is named results.
     useEffect(() => {
         API.getRandomUser().then(data => {
             const listEmployees = data.data.results.map((employee:any) => {
@@ -23,6 +23,7 @@ const Table = () => {
                     street: `${employee.location.street.number} ${employee.location.street.name}`,
                     city: `${employee.location.city}`,
                     state: `${employee.location.state}`,
+                    zipcode: `${employee.location.postcode}`,
                     image: `${employee.picture.thumbnail}`
                 }
             })
@@ -33,17 +34,19 @@ const Table = () => {
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     const imageBodyTemplate = (rowData:any) => {
-        return <img src={`${rowData.image}`} alt={rowData.name} className="product-image" />;
+        return <img src={`${rowData.image}`} alt={rowData.name} className="p-shadow-2" />;
     }
     //console.log(employees);
     return (
         <div>
             <div className="card">
-                <DataTable value={employees} header="Large Table" className="p-datatable-lg p-datatable-striped" removableSort scrollable scrollHeight="200px">
+                {/* The datatable value is the initial value we set up in useState but is then update with the changes made when we create a new array for listEmployees. The field name must match the key name in const listEmployees */}
+                <DataTable value={employees} header="US Employees" className="p-datatable-lg p-datatable-striped" removableSort scrollable>
                     <Column field="name" header="Name" sortable></Column>
-                    <Column field="street" header="Street" sortable></Column>
+                    <Column field="street" header="Street"></Column>
                     <Column field="city" header="City" sortable></Column>
                     <Column field="state" header="State" sortable></Column>
+                    <Column field="zipcode" header="Zip"></Column>
                     <Column field="image" header="Image" body={imageBodyTemplate}></Column>
                 </DataTable>
             </div>
