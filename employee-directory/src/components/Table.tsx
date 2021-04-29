98ef0e734f167add26cfb86c321323442e5cea27
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-04-29 12:27:40 -0400

    Add API endpoint, add api results map for employees

--- employee-directory/src/components/Table.tsx
+++ employee-directory/src/components/Table.tsx
@@ -1,28 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import getRandomUser from "../utils/API";
+
 // Prime React Library
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import ProductService from '../service/ProductService';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+import API from '../utils/API'
 
 const Table = () => {
 
     // First item in array sets the initial state, second item is the function to update it
-    const [products, setProducts] = useState([]);
-    const productService = new ProductService();
+    const [employees, setEmployees] = useState([]);
+    //const productService = new ProductService();
 
     useEffect(() => {
-        productService.getProductsSmall().then(data => setProducts(data));
+        API.getRandomUser().then(data => {
+            const listEmployees = data.data.results.map((employee:any) => {
+                return {
+                    name: `${employee.name.first} ${employee.name.last}`,
+                    street: `${employee.location.street.number} ${employee.location.street.name}`,
+                    city: `${employee.location.city}`,
+                    state: `${employee.location.state}`,
+                    image: `${employee.picture.thumbnail}`
+                }
+            })
+            setEmployees(listEmployees)
+
+
+        });
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    
+    const imageBodyTemplate = (rowData:any) => {
+        return <img src={`${rowData.image}`} alt={rowData.name} className="product-image" />;
+    }
+    //console.log(employees);
     return (
         <div>
             <div className="card">
-                <DataTable value={products} header="Large Table" className="p-datatable-lg p-datatable-striped" removableSort>
-                    <Column field="code" header="Code" sortable></Column>
+                <DataTable value={employees} header="Large Table" className="p-datatable-lg p-datatable-striped" removableSort scrollable scrollHeight="200px">
                     <Column field="name" header="Name" sortable></Column>
-                    <Column field="category" header="Category" sortable></Column>
-                    <Column field="quantity" header="Quantity" sortable></Column>
+                    <Column field="street" header="Street" sortable></Column>
+                    <Column field="city" header="City" sortable></Column>
+                    <Column field="state" header="State" sortable></Column>
+                    <Column field="image" header="Image" body={imageBodyTemplate}></Column>
                 </DataTable>
             </div>
         </div>
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export default {
   getRandomUser: function() {
-    return axios.get("https://randomuser.me/api/?format=json");
+    return axios.get("https://randomuser.me/api/?results=50&nat=us&exc=login")
   }
 };
